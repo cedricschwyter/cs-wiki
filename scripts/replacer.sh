365d141cb0ffabd72ef39d15c1d45086d7a9d227
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-07-07 20:51:34 +0200

    pages: use jekyll with markdown

--- scripts/replacer.sh
+++ scripts/replacer.sh
@@ -5,6 +5,3 @@ find -name '*.md' -exec sed -i 's/<aside>//g' {} \;
 find -name '*.md' -exec sed -i 's/<\/aside>//g' {} \;
 
 (cd ../katex-renderer && find ../docs -name '*.md' -exec cargo run --release -q -- {} \;)
-
-find -name '*.md' -exec sed -i 's/{/\&#123;/g' {} \;
-find -name '*.md' -exec sed -i 's/}/\&#125;/g' {} \;
