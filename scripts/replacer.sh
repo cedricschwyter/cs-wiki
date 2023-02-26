49c5bb1815c4014d5219fcfe07b57245c3fb0192
Cedric Schwyter <cedricschwyter@bluewin.ch>
2023-02-26 22:40:05 +0100

    feat:fix markdown in top-level HTML

--- scripts/replacer.sh
+++ scripts/replacer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 find -name '*.md' -exec sed -i 's/\.md/\.html/g' {} \;
-find -name '*.md' -exec sed -i 's/<aside>/<div class="note">/g' {} \;
+find -name '*.md' -exec sed -i 's/<aside>/<div class="note" markdown="1">/g' {} \;
 find -name '*.md' -exec sed -i 's/<\/aside>/<\/div>/g' {} \;
 
 (cd ../katex-renderer && find ../docs -name '*.md' -exec cargo run --release -q -- {} \;)
