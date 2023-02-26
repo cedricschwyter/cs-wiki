5a090af30ef8e00a0cd90a77fb8b2715edf7b9e8
Cedric Schwyter <cedricschwyter@bluewin.ch>
2023-02-26 21:03:58 +0100

    feat:make notes gray

--- scripts/replacer.sh
+++ scripts/replacer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 find -name '*.md' -exec sed -i 's/\.md/\.html/g' {} \;
-find -name '*.md' -exec sed -i 's/<aside>//g' {} \;
-find -name '*.md' -exec sed -i 's/<\/aside>//g' {} \;
+find -name '*.md' -exec sed -i 's/<aside>/<div class="note">/g' {} \;
+find -name '*.md' -exec sed -i 's/<\/div>//g' {} \;
 
 (cd ../katex-renderer && find ../docs -name '*.md' -exec cargo run --release -q -- {} \;)
 
